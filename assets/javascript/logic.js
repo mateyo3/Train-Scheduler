6852bd5b1b6a06e5ad471ae3557707752ee0117a
mateyo3 <mateoreyes720@gmail.com>
2017-09-30 21:18:08 -0700

    html form and table

---
+++ assets/javascript/logic.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+
+
+	// var randomDate = "02/23/1999";
+ //    var randomFormat = "MM/DD/YYYY";
+ //    var convertedDate = moment(randomDate, randomFormat);
+
+ //    // Using scripts from moment.js write code below to complete each of the following.
+ //    // Console.log to confirm the code changes you made worked.
+
+ //    // 1 ...to convert the randomDate into three other date formats
+ //    console.log(moment(convertedDate).format("MM/DD/YY"));
+ //    console.log(moment(convertedDate).format("MMM Do, YYYY hh:mm:ss"));
+ //    console.log(moment(convertedDate).format("X"));
+ //    console.log("----------------------------------------");
+
+// });
